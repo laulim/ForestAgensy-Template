a89d4e1ab3efd45367a312b03351452a039a7195
Lyudmila <laudylim@yandex.ru>
2018-05-08 16:19:31 +0300

    services ready

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
 
-// Приклеенное меню
-	var nav = $('#menu');
-     
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            nav.addClass("menu-wrap--fixed");
-        } else {
-            nav.removeClass("menu-wrap--fixed");
-        }
-    });
-
 });
