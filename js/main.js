a9a660d30b9df5790365762ba12f50b93a36a08b
Lyudmila <laudylim@yandex.ru>
2018-05-08 20:51:47 +0300

    for bs4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 
+
+//PageScroll2Id
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
+
+	
+//owl.carousel.2
+
+	// $(".owl-carousel").owlCarousel({
+	// 	items: 4,
+	// });
+
+
 });
